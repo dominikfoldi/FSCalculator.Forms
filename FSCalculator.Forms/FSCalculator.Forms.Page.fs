f1b595c3c4e5b3a378d4eee104d9a4827ecc8b9d
Dominik Foldi <dominik.foldi@outlook.com>
2016-11-17 23:46:48 +0100

    Created starter F# Forms app

---
+++ FSCalculator.Forms/FSCalculator.Forms.Page.fs
@@ -0,0 +1,19 @@
+﻿namespace FSCalculator.Forms
+
+open System
+open Xamarin.Forms
+
+type FSCalculatorFormsPage() = 
+    inherit ContentPage()
+
+    do base.Content <- Label(Text = "Hello, F# Xamarin.Forms",
+                             HorizontalOptions = LayoutOptions.Center,
+                             VerticalOptions = LayoutOptions.Center,
+                             FontSize = 24.0,
+                             TextColor = Color.Pink,
+                             FontAttributes = FontAttributes.Italic,
+                             Rotation = -15.0)
+
+type App() = 
+    inherit Application(MainPage = FSCalculatorFormsPage())
+
