15de95910fbebb58fcde4121999a8c367f7f73c8
Dominik Foldi <dominik.foldi@outlook.com>
2016-11-20 12:24:20 +0100

    Adding FSCalculator to the references

--- FSCalculator.Forms/FSCalculator.Forms.Page.fs
+++ FSCalculator.Forms/FSCalculator.Forms.Page.fs
@@ -2,17 +2,24 @@
 
 open System
 open Xamarin.Forms
+open Xamarin.Forms.Xaml
 
 type FSCalculatorFormsPage() = 
     inherit ContentPage()
 
-    do base.Content <- Label(Text = "Hello, F# Xamarin.Forms",
-                             HorizontalOptions = LayoutOptions.Center,
-                             VerticalOptions = LayoutOptions.Center,
-                             FontSize = 24.0,
-                             TextColor = Color.Pink,
-                             FontAttributes = FontAttributes.Italic,
-                             Rotation = -15.0)
+    let _ = base.LoadFromXaml(typeof<FSCalculatorFormsPage>)
+
+    let entry = base.FindByName<Entry>("entry")
+    let listView = base.FindByName<ListView>("listView")
+
+    member this.OnCalculateButtonClicked(sender : Object, args : EventArgs) =
+        let couldParse, n = Int32.TryParse(entry.Text)
+        if (couldParse) then
+            let m = (n + 1) / 2 - 2
+            do listView.ItemsSource <-
+                Array.init(m * m) (fun index -> (index % m + 2) * (index / m + 2))
+                |> Set.ofArray
+                |> Set.difference (Set.ofArray([| 2..n |]))
 
 type App() = 
     inherit Application(MainPage = FSCalculatorFormsPage())
